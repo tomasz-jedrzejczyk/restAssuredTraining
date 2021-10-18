2ea0c1bad1aded44c14cb52c78d8006f08c14495
tomasz-jedrzejczyk <tomasz.jedrzejczyk.dev@gmail.com>
2021-10-18 12:43:35 +0200

    create restAssured test for todois create new project

--- src/test/java/org/example/ProjectCreationTest.java
+++ src/test/java/org/example/ProjectCreationTest.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class ProjectCreationTest {
@@ -7,5 +10,19 @@ public class ProjectCreationTest {
     @Test
     public void userCanCreateAProject(){
 
+        RestAssured
+                .given()
+                    .baseUri("https://api.todoist.com")
+                    .header("Authorization", "Bearer 6e0af658835382fa334b51863752c64b07dcc204")
+                    .header("Content-Type", "application/json")
+                    .body("{\"name\":\"Moj nowy projekt\"}")
+                .when()
+                    .post("/rest/v1/projects")
+                .then()
+                    .assertThat()
+                .statusCode(200)
+                .body("name", Matchers.equalTo("Moj nowy projekt"))
+                .header("Content-Type", Matchers.equalTo("application/json"))
+                    .log().all();
     }
 }
