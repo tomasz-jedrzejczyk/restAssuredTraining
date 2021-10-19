c34e17345fa68d6f69f3e71afc8072f3ca67787d
tomasz-jedrzejczyk <tomasz.jedrzejczyk.dev@gmail.com>
2021-10-19 12:15:06 +0200

    add pom.xml

--- src/test/java/org/example/ProjectCreationTest.java
+++ src/test/java/org/example/ProjectCreationTest.java
@@ -1,13 +1,8 @@
 package org.example;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.example.steps.ProjectSteps;
-import org.junit.Before;
 import org.junit.Test;
 
-import static java.lang.String.format;
-import static org.hamcrest.Matchers.equalTo;
 
 public class ProjectCreationTest extends Base{
 
